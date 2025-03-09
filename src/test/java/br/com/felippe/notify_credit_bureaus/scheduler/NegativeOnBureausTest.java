3bdb30be2cae91bb4db00ddcbb886dfc7494e4bf
felippe Souza <felippe.souza.ti@gmail.com>
2025-03-09 19:49:15 -0300

    Implementação dos testes unitarios

--- src/test/java/br/com/felippe/notify_credit_bureaus/scheduler/NegativeOnBureausTest.java
+++ src/test/java/br/com/felippe/notify_credit_bureaus/scheduler/NegativeOnBureausTest.java
@@ -4,7 +4,6 @@ import br.com.felippe.notify_credit_bureaus.model.DebtsToNegate;
 import br.com.felippe.notify_credit_bureaus.producer.ProduceMessageToBureaus;
 import br.com.felippe.notify_credit_bureaus.producer.dto.MessageDto;
 import br.com.felippe.notify_credit_bureaus.repository.DebtsToNegateRepository;
-import br.com.felippe.notify_credit_bureaus.scheduler.NegativeOnBureaus;
 import br.com.felippe.notify_credit_bureaus.enums.ProcessStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -37,21 +36,19 @@ public class NegativeOnBureausTest {
 
     @BeforeEach
     public void setUp() {
-        // Inicializando o objeto de dívida para o teste
         debt = mock(DebtsToNegate.class);
         when(debt.getId()).thenReturn(UUID.randomUUID()); // Mockando o retorno do ID
     }
 
     @Test
     public void testNegative_whenDebtsFound() {
-        // Dado que temos uma lista de dívidas
         List<DebtsToNegate> debts = Arrays.asList(debt);
         when(repository.findByStatus(ProcessStatus.PENDING)).thenReturn(debts);
 
-        // Chama o método negativo
+
         negativeOnBureaus.negative();
 
-        // Verificando se o método foi chamado
+
         verify(produceMessageToBureaus, times(1)).sendToBureau(any(MessageDto.class));
         verify(debt, times(1)).checkProcessed();
         verify(repository, times(1)).save(debt);
@@ -59,15 +56,17 @@ public class NegativeOnBureausTest {
 
     @Test
     public void testNegative_whenNoDebtsFound() {
-        // Dado que não existem dívidas para processar
+
         when(repository.findByStatus(ProcessStatus.PENDING)).thenReturn(Collections.emptyList());
 
-        // Chama o método negativo
+
         negativeOnBureaus.negative();
 
-        // Verifica que nenhum método foi chamado
+
         verify(produceMessageToBureaus, never()).sendToBureau(any());
         verify(debt, never()).checkProcessed();
         verify(repository, never()).save(any());
     }
+
+
 }
